aaccedac4d9e5147321e625612248e3b269b1276
seanmckee <seanmingmckee@gmail.com>
2023-12-07 14:54:54 -0800

    clerk js set up and public route for / added

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Pen } from "lucide-react";
+import { UserButton, SignInButton, SignUpButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-2-screen flex-col items-center justify-between p-24 gap-4">
+      <UserButton />
       <h3 className="">Welcome to Archive</h3>
       <h1 className="text-4xl font-bold p-3">
         Your Collaborative Document Platform
       </h1>
       <div className="flex gap-x-2">
-        <Button>
-          <Pen className="mr-2 h-4 w-4" />
-          Get Started
-        </Button>
-        <Button variant="outline">Log In</Button>
+        <SignUpButton>
+          <Button>
+            <Pen className="mr-2 h-4 w-4" />
+            Get Started
+          </Button>
+        </SignUpButton>
+
+        <SignInButton>
+          <Button variant="outline">Sign In</Button>
+        </SignInButton>
       </div>
     </main>
   );
