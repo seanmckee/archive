e332591d8aee8bbc2ca582e16086309cdef962e1
seanmckee <seanmingmckee@gmail.com>
2023-12-07 15:21:12 -0800

    fixed auth logic for signed in users

---
+++ app/documents/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Documents Page</h1>
+      <UserButton />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Pen } from "lucide-react";
-import { UserButton, SignInButton, SignUpButton } from "@clerk/nextjs";
+import { UserButton, SignInButton, SignUpButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { userId } = auth();
+  if (userId) {
+    redirect("/documents");
+  }
+
   return (
     <main className="flex min-2-screen flex-col items-center justify-between p-24 gap-4">
-      <UserButton />
       <h3 className="">Welcome to Archive</h3>
       <h1 className="text-4xl font-bold p-3">
         Your Collaborative Document Platform
       </h1>
       <div className="flex gap-x-2">
-        <SignUpButton>
+        <SignUpButton afterSignInUrl="/documents" afterSignUpUrl="/documents">
           <Button>
             <Pen className="mr-2 h-4 w-4" />
             Get Started
